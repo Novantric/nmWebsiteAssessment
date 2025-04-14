da52f2aea6149c35980f538642be023b81f78466
Novantric <whyrlegyge1@gmail.com>
2025-04-14 13:43:32 +0100

    stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,15 @@
-const sidebar = document.getElementById('sidebar')
 
 function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar')
+
     sidebar.classList.toggle('showSidebar');
+
+    // Prevent tabbing through items with inert atrb
+    if (sidebar.classList.contains('showSidebar')) {
+        document.body.classList.add('sidebar-open');
+        mainContent.forEach(element => element.inert = true);
+    } else {
+        document.body.classList.remove('sidebar-open');
+        mainContent.forEach(element => element.inert = false);
+    }
 }
